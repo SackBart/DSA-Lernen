e31b0a1a0a69de6ecac65d6778f14a120c6fefce
SackBart <st192450@stud.uni-stuttgart.de>
2026-02-03 15:44:43 +0100

    Add SelectionSort

---
+++ src/main/java/org/main/examples/lists/Sorting.java
@@ -0,0 +1,24 @@
+package org.main.examples.lists;
+
+import org.main.sorting.SelectionSort;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Sorting {
+    public static void main(String[] args) {
+        Random r = new Random();
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            int random = r.nextInt(100);
+
+            list.add(random);
+        }
+
+        System.out.println("List: " + list);
+        SelectionSort.sortDescending(list);
+        System.out.println("Sorted List: " + list);
+    }
+}
---
+++ src/main/java/org/main/sorting/SelectionSort.java
@@ -0,0 +1,76 @@
+package org.main.sorting;
+
+import java.util.List;
+
+/**
+ * Ascending:
+ * SelectionSort starts with the whole Array then gets the biggest number and decreases the field by one size (right to left)
+ * until the size is 0
+ *
+ * Descending:
+ * Same with Ascending, but it searches for the lowest number
+ */
+
+public class SelectionSort {
+    /**
+     * Sorts the integers inside the array list using selection sort in ascending order
+     * @param list the list to sort
+     * @return the same list but sorted
+     */
+    public static List<Integer> sortAscending(final List<Integer> list) {
+        int p = list.size() - 1;
+
+        while (p > 0) {
+            // Get the index with the highest value inside the current field
+            int currentHighestIndex = 0;
+            for (int i = 0; i <= p; i++) {
+                if (list.get(currentHighestIndex) < list.get(i)) {
+                    currentHighestIndex =  i;
+                }
+            }
+
+            // Switch the index with the highest value on the highest current index
+            int highestValue = list.get(currentHighestIndex);
+            int valueOnP = list.get(p);
+
+            list.set(currentHighestIndex, valueOnP);
+            list.set(p, highestValue);
+
+            // Decrease the field
+            p -= 1;
+        }
+
+        return list;
+    }
+
+    /**
+     * Sorts the integers inside the array list using selection sort in descending order
+     * @param list the list to sort
+     * @return the same list but sorted
+     */
+    public static List<Integer> sortDescending(final List<Integer> list) {
+        int p = list.size() - 1;
+
+        while (p > 0) {
+            // Get the index with the lowest value inside the current field
+            int currentHighestIndex = 0;
+            for (int i = 0; i <= p; i++) {
+                if (list.get(currentHighestIndex) > list.get(i)) { // now it's > instead of <
+                    currentHighestIndex =  i;
+                }
+            }
+
+            // Switch the index with the lowest value with the current highest index
+            int highestValue = list.get(currentHighestIndex);
+            int valueOnP = list.get(p);
+
+            list.set(currentHighestIndex, valueOnP);
+            list.set(p, highestValue);
+
+            // Decrease the field
+            p -= 1;
+        }
+
+        return list;
+    }
+}
