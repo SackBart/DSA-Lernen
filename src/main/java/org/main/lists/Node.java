01d93bd0c032c62767eb938e58c088cb4efc4d44
Erik <erikschaefer0@gmail.com>
2026-02-03 12:45:08 +0100

    node

---
+++ src/main/java/org/main/lists/Node.java
@@ -0,0 +1,13 @@
+package org.main.lists;
+
+public class Node {
+    Node next;
+    Object obj;
+
+    public Node(final Object obj, final Node next) {
+        this.next = next;
+        this.obj = obj;
+    }
+
+
+}
